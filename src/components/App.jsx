f43986a678d7b35641c4f816448d0919e7260d17
Florent <florent.sylejmani@student.uni-pr.edu>
2025-11-25 13:16:37 +0100

    Refactor app structure to use CitiesContext for city data management and enhance component functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
-import { useState } from "react";
-import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "../pages/Product";
 import Pricing from "../pages/Pricing";
 import Homepage from "../pages/Homepage";
@@ -11,46 +9,29 @@ import CityList from "./CityList";
 import CountryList from "./CountryList";
 import City from "./City";
 import Form from "./Form";
+import { CitiesProvider } from "../contexts/CitiesContext";
 
-const BASE_URL = "http://localhost:8000";
 function App() {
-
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(function(){
-    async function fetchCities(){
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("Error fetching cities data");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
   // const x=3;
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="product" element={<Product />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="login" element={<Login />} />
-        <Route index element={<Homepage />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="app" element={<AppLayout />}>
-          <Route index element={<Navigate replace to="cities" />} />
-          <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
-          <Route path='cities/:id' element={<City/>} />
-          <Route path='countries' element={<CountryList cities={cities} isLoading={isLoading} />} />
-          <Route path="form" element={<Form />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="product" element={<Product />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
+          <Route index element={<Homepage />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route path="app" element={<AppLayout />}>
+            <Route index element={<Navigate replace to="cities" />} />
+            <Route path="cities" element={<CityList  />} />
+            <Route path="cities/:id" element={<City />} />
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </CitiesProvider>
   );
 }
 
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,20 +2,26 @@ import Spinner from "./Spinner.jsx";
 import styles from "./CountryList.module.css";
 import CountryItem from "./CountryItem.jsx";
 import Message from "./Message.jsx";
+import { useCities } from "../contexts/CitiesContext.jsx";
 
-function CountriesList({ cities, isLoading }) {
+function CountriesList() {
+  const { cities, isLoading } = useCities();
   if (isLoading) return <Spinner />;
   if (!cities.length) return <Message message="No cities added yet" />;
 
   const countries = cities.reduce((arr, city) => {
-    if(!arr.map(el=>el.country).includes(city.country))
-      return [...arr, {country: city.country, emoji:city.emoji}] 
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
     else return arr;
   }, []);
 
-  return <ul className={styles.countryList}>
-    {countries.map((country) => <CountryItem country={country} key={country.country} />)}
-  </ul>;
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
+      ))}
+    </ul>
+  );
 }
 
 export default CountriesList;
